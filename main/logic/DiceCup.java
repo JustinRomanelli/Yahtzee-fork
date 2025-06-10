f0261756ce8130e186a28ba50b0c9c2e5b9f56b4
JustinRomanelli <justin117247@student-schoolsofwestfield.org>
2025-06-10 12:22:08 -0400

    Commented out old code to avoid errors.

--- main/logic/DiceCup.java
+++ main/logic/DiceCup.java
@@ -23,33 +23,33 @@ public class DiceCup {
         return (int) (Math.random() * 6) + 1;
     }
 
-    public int[] rerollDice() {
-        for (int i = 0; i < hold.length; i++) {
-            if (!hold[i]) {
-                hand[i] = rollDie();
-            }
-
-            int index = 0;
-
-            for (int j = 0; j < hand.length; j++) {
-            
-                if (hand[j] != 0) {
-                    index++;
-                }
-            }
-
-            int[] newArray = new int[index];
-
-            for (int k = 0; k < hand.length; k++) {
-                if (hand[k] != 0) {
-                    newArray[k] = hand[k];
-                }
-                else {
-                    if (k > 0) {
-                        k--;
-                    }
-                }
-            }
-        }
-    }
+    // public int[] rerollDice() {
+    //     for (int i = 0; i < hold.length; i++) {
+    //         if (!hold[i]) {
+    //             hand[i] = rollDie();
+    //         }
+    //
+    //         int index = 0;
+    //
+    //         for (int j = 0; j < hand.length; j++) {
+    //
+    //             if (hand[j] != 0) {
+    //                 index++;
+    //             }
+    //         }
+    //
+    //         int[] newArray = new int[index];
+    //
+    //         for (int k = 0; k < hand.length; k++) {
+    //             if (hand[k] != 0) {
+    //                 newArray[k] = hand[k];
+    //             }
+    //             else {
+    //                 if (k > 0) {
+    //                     k--;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
